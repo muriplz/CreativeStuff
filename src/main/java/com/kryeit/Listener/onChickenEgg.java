e6872f7a00580b612eee80fd1a0747dbe1b074b1
muriplz <46825658+muriplz@users.noreply.github.com>
2023-07-06 04:16:58 +0200

    chickens

--- src/main/java/com/kryeit/Listener/onChickenEgg.java
+++ src/main/java/com/kryeit/Listener/onChickenEgg.java
@@ -1,26 +1,29 @@
 package com.kryeit.Listener;
 
 import com.griefdefender.api.GriefDefender;
-import com.griefdefender.api.User;
 import com.griefdefender.api.claim.Claim;
-import com.griefdefender.api.claim.TrustType;
-import com.griefdefender.api.claim.TrustTypes;
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class onChickenEgg implements Listener {
 
     @EventHandler
     public void onEggDrop(EntityDropItemEvent event) {
-        if (event.getEntity() instanceof Chicken && event.getItemDrop().getItemStack().getType().name().equals("EGG")) {
+        if (event.getEntity() instanceof Chicken && event.getItemDrop().getItemStack().getType().name().equalsIgnoreCase("EGG")) {
             event.setCancelled(true);
         }
     }
@@ -28,31 +31,44 @@ public class onChickenEgg implements Listener {
     @EventHandler
     public void onEggThrow(PlayerEggThrowEvent event) {
         Claim claim = GriefDefender.getCore().getClaimAt(event.getEgg().getLocation());
-// && claim.isUserTrusted(event.getPlayer().getUniqueId(), TrustTypes.BUILDER)
+
         assert claim != null;
-        if(!claim.isWilderness() ) {
-            event.setHatching(false); // Cancel the random hatching
-            event.setNumHatches((byte) 1); // Set the number of chickens to spawn to 1
-            Chicken chicken = event.getPlayer().getWorld().spawn(event.getEgg().getLocation(), Chicken.class);
-            event.getEgg().remove(); // Remove the egg entity
+        if (claim.isWilderness()) return;
+
+        event.setHatching(false); // Cancel the random hatching
+
+        Random random = new Random();
+        boolean shouldSpawnChicken = random.nextBoolean(); // 50% chance of being true
+
+        if (shouldSpawnChicken) {
+            event.getPlayer().getWorld().spawn(event.getEgg().getLocation(), Chicken.class).setBaby();
         }
+
     }
 
     @EventHandler
     public void onChickenDeath(EntityDeathEvent event) {
-        Claim claim = GriefDefender.getCore().getClaimAt(event.getEntity().getLocation());
-        if(claim == null) return;
-        if (event.getEntity() instanceof Chicken && !claim.isWilderness()) {
+        if (event.getEntity() instanceof Chicken && !Objects.requireNonNull(GriefDefender.getCore().getClaimAt(event.getEntity().getLocation())).isWilderness() ) {
+            Player killer = event.getEntity().getKiller();
+            int lootingLevel = 0;
+            if (killer != null) {
+                lootingLevel = killer.getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS);
+            }
             int numEggs = 0;
             double rand = Math.random();
-            if (rand < 0.90) {
-                numEggs = 1;
+            if (rand < 0.40 + 0.10 * lootingLevel) {
+                numEggs = 1 + lootingLevel;
+            } else if (rand < 0.70 + 0.10 * lootingLevel) {
+                numEggs = 2 + lootingLevel;
+            } else if (rand < 0.80 + 0.05 * lootingLevel) {
+                numEggs = 3 + lootingLevel;
+            } else if (rand < 0.90) {
+                numEggs = 4 + lootingLevel;
             } else {
-                numEggs = 2;
+                numEggs = 5 + lootingLevel;
             }
             ItemStack eggStack = new ItemStack(org.bukkit.Material.EGG, numEggs);
             event.getDrops().add(eggStack);
         }
     }
-
 }
